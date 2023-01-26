d5933141070138f2f93e70c5793dea96be22b1f6
tohsaka888 <156132264@qq.com>
2023-01-26 14:22:02 +0800

    ✨ feat: add type config

--- types/index.ts
+++ types/index.ts
@@ -1,6 +1,30 @@
+import { Object3DNode, MaterialNode } from "@react-three/fiber";
+import { MeshLineGeometry, MeshLineMaterial } from "meshline";
+import { ShaderMaterial } from "three";
+
 export type ScreenSizeProps = {
   width: number;
   height: number;
 };
 
 export type ScreenSizeList = "xs" | "sm" | "md" | "lg" | "xl";
+
+export type FatLineProps = {
+  curve: any;
+  width?: string | number;
+  color: string;
+};
+
+export type FirefliesProps = {
+  count: number;
+  colors: string[];
+  radius: number;
+};
+
+declare module "@react-three/fiber" {
+  interface ThreeElements {
+    meshLineGeometry: Object3DNode<MeshLineGeometry, typeof MeshLineGeometry>;
+    meshLineMaterial: MaterialNode<MeshLineMaterial, typeof MeshLineMaterial>;
+    layerMaterial: MaterialNode<ShaderMaterial, typeof ShaderMaterial>
+  }
+}
