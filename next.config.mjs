a04d3c80103ecc9e6299bdf78890e9d5557ce1be
tohsaka888 <156132264@qq.com>
2023-01-26 10:10:10 +0800

    ✨ feat: use `mjs` config and config mdx plugins.
    To use `remark-gfm` must use `mjs` config file, [see detail](https://nextjs.org/docs/advanced-features/using-mdx#setup-nextmdx-in-nextjs)

---
+++ next.config.mjs
@@ -0,0 +1,60 @@
+/** @type {import('next').NextConfig} */
+
+import WithMDX from "@next/mdx";
+import toc from "@jsdevtools/rehype-toc";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeCodeTitle from "rehype-code-title";
+import rehypePrism from "rehype-prism-plus";
+import remarkCodeSandBox from "remark-codesandbox";
+import remarkGfm from "remark-gfm";
+import remarkHint from "remark-hint";
+import rehypeSlug from "rehype-slug";
+
+const withMDX = WithMDX({
+  extension: /\.mdx?$/,
+  options: {
+    // If you use remark-gfm, you'll need to use next.config.mjs
+    // as the package is ESM only
+    // https://github.com/remarkjs/remark-gfm#install
+    remarkPlugins: [
+      [remarkCodeSandBox],
+      [remarkGfm],
+      [remarkHint],
+    ],
+    rehypePlugins: [
+      [
+        rehypeAutolinkHeadings,
+        {
+          behavior: "wrap",
+          test: ["h1", "h2", "h3"],
+        },
+      ],
+      rehypeSlug,
+      [
+        toc,
+        {
+          nav: true,
+          headings: ["h1", "h2", "h3"],
+          
+        },
+      ],
+      [rehypePrism],
+    ],
+    // If you use `MDXProvider`, uncomment the following line.
+    providerImportSource: "@mdx-js/react",
+  },
+});
+
+const nextConfig = {
+  // Configure pageExtensions to include md and mdx
+  pageExtensions: ["ts", "tsx", "js", "jsx", "md", "mdx"],
+  // Optionally, add any other Next.js config below
+  reactStrictMode: true,
+  compiler: {
+    // ssr and displayName are configured by default
+    styledComponents: true,
+  },
+};
+
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig);
